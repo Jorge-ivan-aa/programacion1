6fd9e7b7e084530f8a23f27f848d7dacde8b4171
PAISITAPROGRAMADOR <sjorgeesteban09@gmail.com>
2023-11-12 19:43:12 -0500

    Se crea los atributos y constructores de la clase de directorio y vuelo

--- Aeropuerto/src/main/java/aeropuerto/modules/Directorio.java
+++ Aeropuerto/src/main/java/aeropuerto/modules/Directorio.java
@@ -1,5 +1,24 @@
 package aeropuerto.modules;
 
-public interface Directorio {
+import java.util.ArrayList;
+
+public class Directorio {
+    private ArrayList<String> aerolineas;
+
+    public Directorio(ArrayList<String> aerolineas) {
+        this.aerolineas = aerolineas;
+    }
+
+    public ArrayList<String> getAerolineas() {
+        return aerolineas;
+    }
+
+    @Override
+    public String toString() {
+        return "Nuestro directorio de aerolineas:" + aerolineas;
+    }
+
     
+
+
 }
--- Aeropuerto/src/main/java/aeropuerto/modules/Vuelo.java
+++ Aeropuerto/src/main/java/aeropuerto/modules/Vuelo.java
@@ -1,5 +1,37 @@
 package aeropuerto.modules;
 
 public class Vuelo {
+    private String aerolinea, categoria, estado, terminal, puertaEmbarque, fechaDespegue, fechaAterrizaje, ruta;
+    private int numVuelo, numPasajeros;
     
+    
+    public Vuelo(int numVuelo, String aerolinea, String categoria, String estado, String terminal, String puertaEmbarque,
+            String fechaDespegue, String fechaAterrizaje, String ruta, int numPasajeros) {
+       
+        this.numVuelo = numVuelo;
+        this.aerolinea = aerolinea;
+        this.categoria = categoria;
+        this.estado = estado;
+        this.terminal = terminal;
+        this.puertaEmbarque = puertaEmbarque;
+        this.fechaDespegue = fechaDespegue;
+        this.fechaAterrizaje = fechaAterrizaje;
+        this.ruta = ruta;
+        this.numPasajeros = numPasajeros;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Numero de vuelo: " + this.numVuelo + "aerolinea: " + this.aerolinea + "categoria: " +
+        this.categoria + "estado: " + this.estado + "terminal: " + this.terminal + "puertaEmbarque: " + 
+        this.puertaEmbarque + "fechaDespegue: " + this.fechaDespegue +"fechaAterrizaje: " + 
+        this.fechaAterrizaje + "ruta: " + this.ruta + "numVuelo: " + this.numVuelo + 
+        "numPasajeros: " + this.numPasajeros;
+    }
+    
+    
+
+
+
 }
